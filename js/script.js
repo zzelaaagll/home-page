8ec556d15ce9e4ec93650a9efa2605e738d1bde1
DESKTOP-BVNGPMK\TianTai <admin@localhost>
2023-05-26 16:13:04 +0800

    更新

---
+++ js/script.js
@@ -0,0 +1,125 @@
+$(function () {
+	var base = "THGrrlc3zawzZthwZXGYPik7xR3UdU/x4BqnpWaSDKNqYNbRTtSEuFjnmb9xdNyq";
+	var newDate = {
+		getTime: function () {
+			let date = new Date();
+			return date.getHours() + ":" + (date.getMinutes() >= 10 ? date.getMinutes() : "0" + date.getMinutes());
+		}
+		,getToday: function () {
+			let date = new Date();
+			return date.getMonth() + "月" + date.getDate() + "日";
+		}
+		,getWeek: function () {
+			let date = new Date();
+			let week = "";
+			switch (date.getDay()) {
+				case 0: return "星期日";
+				case 1: return "星期一";
+				case 2: return "星期二";
+				case 3: return "星期三";
+				case 4: return "星期四";
+				case 5: return "星期五";
+				case 6: return "星期六";
+			}
+		}
+	};
+	var hibernate = {
+		time: 0,
+		timer: null,
+		start: function () {
+			clearTimeout(hibernate.timer);
+			hibernate.timer = setTimeout(function () {
+				hibernate.time++;
+				if (hibernate.time >= 10) {
+					closeGate();
+				}else {
+					hibernate.start();
+				}
+			}, 1000);
+		},
+		end: function () {
+			clearTimeout(hibernate.timer);
+			hibernate.init();
+		},
+		init: function () {
+			hibernate.time = 0;
+		}
+	};
+	function openGate () {
+		showLoginInput();
+		$(".gate").css({display:"none"});
+		$(".login-input").focus();
+		$(".login-input").val("");
+		hibernate.start();
+	}
+	function closeGate () {
+		showLoginInput();
+		$(".gate").css({display:"block"});
+		$(".login-input").blur();
+		$(".login-input").val("");
+		hibernate.end();
+		
+	}
+	function showLoginInput () {
+		$(".login-no-password").css({display:"none"});
+		$(".login").css({display:'block'});
+		$(".login-input").focus();
+		$(".login-input").val("");
+	}
+	function showNoPassword () {
+		$(".login-no-password").css({display:"block"});
+		$(".login").css({display:'none'});
+		$(".login-input").blur();
+	}
+	function showTime () {
+		$(".show-time>p").eq(0).html(newDate.getTime());
+		$(".show-time>p").eq(1).html(newDate.getToday() + ", " + newDate.getWeek());
+		setTimeout(showTime, 1000);
+	}
+	function check () {
+		let p = $(".login-input").val();
+		let d = decrypt(base, p.MD5(16), p.MD5(16));
+		if (d) {
+			window.location.href = d.answer;
+		}else {
+			showNoPassword();
+		}
+	}
+	$("body").keyup(function (e) {
+		hibernate.init();
+		if (e.keyCode === 13 && $(".gate").css("display") !== "none") {
+			openGate();
+			return;
+		}
+		if (e.keyCode === 27) {
+			closeGate();
+			return;
+		}
+		if (e.keyCode === 13 && $(".login-no-password").css("display") !== "none") {
+			showLoginInput();
+			return;
+		}
+		if (e.keyCode === 13 && $(".login").css("display") !== "none") {
+			check();
+			return;
+		}
+	});
+	$("body").on("mouseout click", function () {
+		hibernate.init();
+	});
+	$(".login>button").click(function(){
+		check();
+	});
+	$(".gate").click(function(){
+		openGate();
+	});
+	$(".login-no-password>p>button").click(function () {
+		showLoginInput();
+	});
+	$(".login-input").blur(function () {
+		if ($(".login").css("display") !== "none") {
+			$(".login-input").focus();
+		}
+	});
+	showTime();
+});
---
+++ js/tool.js
@@ -0,0 +1,32 @@
+
+function decrypt (str, key, iv) {
+	try {
+		const bytes = CryptoJS.AES.decrypt(str, CryptoJS.enc.Utf8.parse(key), {
+		  iv: CryptoJS.enc.Utf8.parse(iv),
+		  mode: CryptoJS.mode.CBC,
+		  padding: CryptoJS.pad.Pkcs7
+		});
+		const decryptedText = bytes.toString(CryptoJS.enc.Utf8);
+		return JSON.parse(decryptedText);
+	}catch (e) {
+		return "";
+	}
+}
+
+function encrypt (json, key, iv) {
+	try {
+		const plaintext = JSON.stringify(json);
+		const ciphertext = CryptoJS.AES.encrypt(plaintext, CryptoJS.enc.Utf8.parse(key), {
+		  iv: CryptoJS.enc.Utf8.parse(iv),
+		  mode: CryptoJS.mode.CBC,
+		  padding: CryptoJS.pad.Pkcs7
+		});
+		return ciphertext.toString();
+	}catch (e) {
+		return "";
+	}
+}
+
+function base64Decode (txt) {
+	return {pw: "\u590f\u6d1b\u7279\u70e6\u607c", u: "#"};
+}
