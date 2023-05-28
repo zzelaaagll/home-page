f9210fa7710b881f89f997e8a401931fcbc1b53f
DESKTOP-I4O0OU0\Y <y@localhost>
2023-05-28 20:40:22 +0800

    更新

--- js/tool.js
+++ js/tool.js
@@ -1,13 +1,15 @@
 
 // 恭喜你找到这里，然而并没有什么用╮(๑•́ ₃•̀๑)╭
-function base64Decode (txt) {
+$.extend({
+	base64Decode:function(txt) {
 
-	if (txt !== undefined && txt !== "") {
-		return {pw: "\u590f\u6d1b\u7279\u70e6\u607c", u: "#"};
-	}else {
-		return "";
+		if (txt !== undefined && txt !== "") {
+			return {pw: "\u590f\u6d1b\u7279\u70e6\u607c", u: "#"};
+		}else {
+			return "";
+		}
 	}
-}
+});
 
 function decrypt (str, key, iv) {
 
